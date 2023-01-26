4582a0f1f3b93026b607df001b1e629fa4633db7
0xAnthonyW <123592619+0xAnthonyW@users.noreply.github.com>
2023-01-26 15:46:33 +0000

    renamed var $in to studentAccount

--- User Account/User Account.ps1
+++ User Account/User Account.ps1
@@ -4,11 +4,11 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit
 }
 #Get-CimInstance -Class Win32_UserProfile | Where-Object { $_.LocalPath.split('\')[-1] -like 'STU*' } | Remove-CimInstance
-$in = Read-host "Enter Student Account Name"
+$studentAccount = Read-host "Enter Student Account Name"
 #$Password = Read-Host -AsSecureString
 $StuPassword = "Student" | ConvertTo-SecureString -AsPlainText -Force
-New-LocalUser -Name "$in" -Description "$in" -Password $StuPassword -Verbose
-Add-LocalGroupMember -Group "Users" -Member "$in"
-Set-LocalUser -Name "$in"
-net user $in /logonpasswordchg:yes
+New-LocalUser -Name "$studentAccount" -Description "$studentAccount" -Password $StuPassword -Verbose
+Add-LocalGroupMember -Group "Users" -Member "$studentAccount"
+Set-LocalUser -Name "$studentAccount"
+net user $studentAccount /logonpasswordchg:yes
 PAUSE
