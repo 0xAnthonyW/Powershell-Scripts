e8fb1c69f9b5cdb9f23910264094f22e6b397ca7
0xAnthonyW <123592619+0xAnthonyW@users.noreply.github.com>
2023-01-26 15:28:59 +0000

    added user change password on login

--- User Account/User Account.ps1
+++ User Account/User Account.ps1
@@ -1,5 +1,6 @@
 #Created by Anthony
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) 
+{
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit
 }
 #Get-CimInstance -Class Win32_UserProfile | Where-Object { $_.LocalPath.split('\')[-1] -like 'STU*' } | Remove-CimInstance
@@ -9,4 +10,5 @@ $StuPassword = "Student" | ConvertTo-SecureString -AsPlainText -Force
 New-LocalUser -Name "$in" -Description "$in" -Password $StuPassword -Verbose
 Add-LocalGroupMember -Group "Users" -Member "$in"
 Set-LocalUser -Name "$in"
+net user $in /logonpasswordchg:yes
 PAUSE
