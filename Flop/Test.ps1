b1d1b807259b731ebc1cb03b9bcf6289b1aaddda
0xAnthonyW <123592619+0xAnthonyW@users.noreply.github.com>
2023-01-27 22:30:13 +0000

    progress not yet done

--- Flop/Test.ps1
+++ Flop/Test.ps1
@@ -1,6 +1,23 @@
 #work in progress more to add
+#todo add  task password expire and execute it, then run flip me. thats all for now. maybe work on recycle bin again...
 #dont run as admin
-$var = '.\Test2.ps1'
-Start-Process Powershell "-ExecutionPolicy Bypass -File $var"
-Read-Host "Can you see me"
+# $var = '.\Test2.ps1'
+$UsbPath = 'D:\PassExpire'
+$Destination = 'C:\Users\admin\Desktop'
+$PassExpirePath = 'C:\Users\admin\Desktop\PassExpire'
+
+if (Test-Path $PassExpirePath) 
+{
+    Remove-Item -Path $PassExpirePath -Force -Recurse
+    Copy-Item -Path $UsbPath -Recurse -Destination $Destination
+    (Get-ChildItem $Destination -Recurse).FullName
+}
+else
+{
+    Copy-Item -Path $UsbPath -Recurse -Destination $Destination
+    (Get-ChildItem $Destination -Recurse).FullName
+}
 PAUSE
+# Start-Process Powershell "-ExecutionPolicy Bypass -File $var"
+# Start-Sleep -Seconds 30
+# Read-Host "Can you see me"
