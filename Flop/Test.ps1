4ab4abadef08ea1d73669a25030de73a0bd965c5
0xAnthonyW <123592619+0xAnthonyW@users.noreply.github.com>
2023-02-03 21:31:57 +0000

    Updates WIP

--- Flop/Test.ps1
+++ Flop/Test.ps1
@@ -1,10 +1,11 @@
-#work in progress more to add
-#todo add  task password expire and execute it, then run flip me. thats all for now. maybe work on recycle bin again...
 #dont run as admin
-# $var = '.\Test2.ps1'
+#work in progress more to add
+#todo maybe work on recycle bin again...
+$var = 'C:\Users\admin\Desktop\PassExpire\TaskPasswordExpire.ps1'
+$var2 = 'C:\Users\admin\Desktop\Software\FlipMe.ps1'
 $UsbPath = 'D:\PassExpire'
 $Destination = 'C:\Users\admin\Desktop'
-$PassExpirePath = 'C:\Users\admin\Desktop\PassExpire'
+$PassExpirePath = 'C:\Users\admin\Desktop\PassExpire' #PassExpire path is still set to desktop not in a folder
 
 if (Test-Path $PassExpirePath) 
 {
@@ -17,7 +18,7 @@ else
     Copy-Item -Path $UsbPath -Recurse -Destination $Destination
     (Get-ChildItem $Destination -Recurse).FullName
 }
+Start-Process Powershell "-ExecutionPolicy Bypass -File $var"
+Start-Sleep -Seconds 20
+Start-Process Powershell "-ExecutionPolicy Bypass -File $var2"
 PAUSE
-# Start-Process Powershell "-ExecutionPolicy Bypass -File $var"
-# Start-Sleep -Seconds 30
-# Read-Host "Can you see me"
