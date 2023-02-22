0197a38ab1939d79c5a0022ba45d359e91acc544
0xAnthonyW <123592619+0xAnthonyW@users.noreply.github.com>
2023-02-22 20:40:20 +0000

    final fix

--- Flop/Virus Task Detection.ps1
+++ Flop/Virus Task Detection.ps1
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Run PowerShell as Admin.
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) 
 {
@@ -46,24 +45,3 @@ else
         Write-Host "The executable file SWUpdater.exe does not exist in the user's profile folder." -ForegroundColor Green
     }
 }
-=======
-if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{
-    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit
-}
-$taskName = 'Wavesor*'
-$tasks = Get-ScheduledTask -TaskName $taskName -ErrorAction SilentlyContinue
-if ($tasks)
-{
-    Write-Host "The following tasks were found with the name '$taskName':" -ForegroundColor Yellow
-    $tasks | ForEach-Object {
-        Write-Host $_.TaskName
-    }
-    Read-Host "Press Enter to continue."
-}
-else
-{
-    Write-Host "No tasks were found with the name '$taskName'." -ForegroundColor Green
-    Read-Host "Press Enter to continue."
-}
->>>>>>> 6115bf0643fa15723143b1a52134158b5ed28ef8
