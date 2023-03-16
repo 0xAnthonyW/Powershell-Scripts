18f1d40266e4f7a08440a464b9e0e740e75558ab
0xAnthonyW <123592619+0xAnthonyW@users.noreply.github.com>
2023-03-16 07:48:56 -0500

    Reverted back, not being used Proof Of Concept

--- Flop/Test.ps1
+++ Flop/Test.ps1
@@ -1,24 +1,6 @@
+#reverted back, isnt being used was a Proof of Concept
 #dont run as admin
-#work in progress more to add
-#todo maybe work on recycle bin again...
-$var = 'C:\Users\admin\Desktop\PassExpire\TaskPasswordExpire.ps1'
-$var2 = 'C:\Users\admin\Desktop\Software\FlipMe.ps1'
-$UsbPath = 'D:\PassExpire'
-$Destination = 'C:\Users\admin\Desktop'
-$PassExpirePath = 'C:\Users\admin\Desktop\PassExpire' #PassExpire path is still set to desktop not in a folder
-
-if (Test-Path $PassExpirePath) 
-{
-    Remove-Item -Path $PassExpirePath -Force -Recurse
-    Copy-Item -Path $UsbPath -Recurse -Destination $Destination
-    (Get-ChildItem $Destination -Recurse).FullName
-}
-else
-{
-    Copy-Item -Path $UsbPath -Recurse -Destination $Destination
-    (Get-ChildItem $Destination -Recurse).FullName
-}
+$var = '.\Test2.ps1'
 Start-Process Powershell "-ExecutionPolicy Bypass -File $var"
-Start-Sleep -Seconds 20
-Start-Process Powershell "-ExecutionPolicy Bypass -File $var2"
+Read-Host "Can you see me"
 PAUSE
