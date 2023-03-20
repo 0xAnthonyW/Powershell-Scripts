cc9b078f368119958d6ef98e712da0a5309ece80
0xAnthonyW <123592619+0xAnthonyW@users.noreply.github.com>
2023-03-20 11:09:26 -0500

    Update AddCopyFilePathContextMenu.ps1

--- CopyFilePath/AddCopyFilePathContextMenu.ps1
+++ CopyFilePath/AddCopyFilePathContextMenu.ps1
@@ -6,13 +6,14 @@ if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
 $regPath = "HKCU:\Software\Classes\*\shell\Copy File Path"
 
 # Create the "Copy File Path" key
+Write-Output "Creating 'Copy File Path' key..."
 New-Item -Path $regPath -Force | Out-Null
 
 # Create the "command" subkey
+Write-Output "Creating 'command' subkey..."
 New-Item -Path "$regPath\command" -Force | Out-Null
 
 # Set the default value of the "command" subkey to the PowerShell command that copies the file path to the clipboard
+Write-Output "Setting default value of 'command' subkey..."
 Set-ItemProperty -Path "$regPath\command" -Name "(Default)" -Value "cmd.exe /c echo.|set /p=%1|clip"
-
-
-Read-Host "Done.."
+Read-Host "Done! Press any key to exit..."
