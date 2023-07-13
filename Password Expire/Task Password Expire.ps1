17df777a777623803cdb3583b30225db5e97b5c2
0xAnthonyW <123592619+0xAnthonyW@users.noreply.github.com>
2023-07-13 11:46:06 -0500

    updated task run setting testing

--- Password Expire/Task Password Expire.ps1
+++ Password Expire/Task Password Expire.ps1
@@ -1,14 +1,14 @@
 # Created By Anthony
-# Task Password Expire
+# Task Password Expire v1.1
 # Sets up Scheduled Task to run the PasswordExpire Script in the background.
 # Run PowerShell as Admin
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 {
     Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit
 }
-
 $action = New-ScheduledTaskAction -Execute "powershell" -Argument "-ExecutionPolicy Bypass -file C:\Users\admin\Desktop\PasswordExpire.ps1"
+$tasksetting = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd
 $trigger = New-ScheduledTaskTrigger -AtLogon
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -RunLevel Highest
-$task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger
+$task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $tasksetting
 Register-ScheduledTask 'PassExpire' -InputObject $task
